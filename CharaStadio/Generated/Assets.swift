262fae4c004988a44ee4ee94a0f815e39b9a5155
Fuyan777 <fuyamiwa713@gmail.com>
2021-02-19 20:19:46 +0900

    [add]SwiftgenでimageAssetの生成

--- CharaStadio/Generated/Assets.swift
+++ CharaStadio/Generated/Assets.swift
@@ -26,6 +26,7 @@ internal enum Asset {
   internal static let textColor = ColorAsset(name: "TextColor")
   internal static let viewBgColor = ColorAsset(name: "ViewBgColor")
   internal static let noImage = ImageAsset(name: "noImage")
+  internal static let yanartLogo = ImageAsset(name: "yanart-logo")
 }
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
 
