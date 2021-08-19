3885b1c1b6dfb5ae4a693e228f97bc252c7fd04f
Fuyan777 <fuyamiwa713@gmail.com>
2021-08-19 09:37:20 +0900

    Add: interactorの実装

--- CharaStadio/Modules/View/CharaList/CharaListInteractor.swift
+++ CharaStadio/Modules/View/CharaList/CharaListInteractor.swift
@@ -8,9 +8,17 @@
 import Foundation
 
 protocol CharaListInteractorInterface {
-    
+    func fetchCharaList(_ completion: @escaping ((Result<[CharaEntity], Error>) -> Void))
 }
 
 final class CharaListInteractor: CharaListInteractorInterface {
+    private let firebaseRepository: FirebaseRepositoryProtocol
     
+    init(firebaseRepository: FirebaseRepositoryProtocol = FirebaseRepository()) {
+        self.firebaseRepository = firebaseRepository
+    }
+    
+    func fetchCharaList(_ completion: @escaping ((Result<[CharaEntity], Error>) -> Void)) {
+        firebaseRepository.getChara { completion($0) }
+    }
 }
